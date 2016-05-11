45983adc193728a073fd02522469e22fd9d909f4
lawrencegomez <lfg2305@gmail.com>
2016-05-10 22:05:04 -0700

    added some explanations of my code

--- d02/calculator/app.js
+++ d02/calculator/app.js
@@ -1,5 +1,7 @@
 
-
+// Tried my best to use an object to store all of my data
+// in lieu of having a bunch of difference variables and arrays to call separately.
+// There are a few functions within it but its mainly just used like a dictionary.
 var calc = {
     buttonsArr: document.querySelectorAll('.num'),
     operatorsArr: document.querySelectorAll('.operator'),
@@ -14,12 +16,17 @@ var calc = {
     screens: document.querySelector('input')
 }
 
+// This function calls upon the calculation array and the input screen and erases
+// the contents of each so that a fresh calc can be run.
 function clear() {
   calc.calculation = []
   calc.screens.value = ''
   calc.screens.placeholder = 'hello'
 }
 
+// This initializes the click events for all of the buttons on the screen.
+// The buttons are in a few difference arrays and objects so there are a few for loops
+ // for the arrays and just a simple event for the other two
 function init() {
   // For loop for the number buttons
     for(var i=0; i < calc.buttonsArr.length; i ++) {
@@ -27,20 +34,27 @@ function init() {
       calc.screens.value += this.innerHTML
       })
     }
-    // For loop for the operator buttons
+
+    // For loop for the operator buttons.
+    // This adds the event listener and then prints the value of the buttons that
+    // are pressed onto the input screen. Then, the input screen value is taken and
+    // thrown into an arry called "calculation"
     for(var x=0; x < calc.operatorsArr.length; x ++ ) {
       calc.operatorsArr[x].addEventListener('click', function() {
       calc.calculation.push(calc.screens.value)
       calc.calculation.push(this.innerHTML)
       calc.screens.value = ''
       calc.screens.placeholder = ''
-      console.log(calc.calculation)
       })
     }
+
     // added event listener for the clear button
     calc.clear.addEventListener('click', clear)
 
-    // added event listener for the equals button
+    // added event listener for the equals button that includes all of the if statements
+    // to tell the computer what operator to use. It takes the numbers from the
+    // calculation array and then performs one of the functions on them
+    // and then clears the calculation array so new numbers can be entered
     calc.equals.addEventListener('click', function() {
       if (calc.calculation[1] == calc.operatorsArr[1].innerHTML) {
           calc.calculation.push(calc.screens.value)
@@ -65,7 +79,8 @@ function init() {
     })
   }
 
-init();
+init(); 
+// Initialize the code so the buttons and code are all ready to go once the screen loads
 
 
 
