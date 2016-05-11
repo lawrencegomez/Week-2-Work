37c446829badf05a88f94fa89c6a2946188532b4
lawrencegomez <lfg2305@gmail.com>
2016-05-10 17:19:04 -0700

    calculator works for single digit numbers and it only console.logs the answer ..WAHOOO"

--- d02/calculator/app.js
+++ d02/calculator/app.js
@@ -5,11 +5,11 @@
 
 var calc = {
     buttonsArr: document.querySelectorAll('.num'),
-    operatorsArr: document.querySelectorAll('operator'),
+    operatorsArr: document.querySelectorAll('.operator'),
     clear: document.querySelector('#clear'),
     calculation: [],
     operations: { add: function(a,b) { return a+b},
-                  subtact: function(a,b) { return a-b},
+                  subtract: function(a,b) { return a-b},
                   multiply: function(a,b) {return a*b},
                   divide: function(a,b) {return a/b}
               },
@@ -26,17 +26,30 @@ function init() {
     }
     // For loop for the operator buttons
     for(var x=0; x < calc.operatorsArr.length; x ++ ) {
-        calc.operatorsArr[x].addEventListener('click', function() {
-        calc.calulation.push(this.innerHTML)
-        console.log(calc.calculation)
+      calc.operatorsArr[x].addEventListener('click', function() {
+      calc.calculation.push(this.innerHTML)
+      console.log(calc.calculation)
       })
     }
+    // added event listener for the clear button
     calc.clear.addEventListener('click', function() {
       calc.calculation = []
       console.log('Emptied the array')
     })
+    // added event listener for the equals button
     calc.equals.addEventListener('click', function() {
-      console.log('Its about to go down')
+        if (calc.calculation[1] == calc.operatorsArr[1].innerHTML) {
+        console.log(calc.operations.add(Number(calc.calculation[0]),Number(calc.calculation[2])))
+      }
+      else if (calc.calculation[1] == calc.operatorsArr[0].innerHTML) {
+        console.log(calc.operations.subtract(Number(calc.calculation[0]),Number(calc.calculation[2])))
+      }
+      else if (calc.calculation[1] == calc.operatorsArr[3].innerHTML) {
+        console.log(calc.operations.multiply(Number(calc.calculation[0]),Number(calc.calculation[2])))
+      }
+      else if (calc.calculation[1] == calc.operatorsArr[2].innerHTML) {
+        console.log(calc.operations.divide(Number(calc.calculation[0]),Number(calc.calculation[2])))
+      }
     })
   }
 
